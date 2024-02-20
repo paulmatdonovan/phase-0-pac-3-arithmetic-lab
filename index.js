e02138df72500e9d5ea47fd093fea33d8367e9bc
Paul Donovan <paul.m.donovan@outlook.com>
2024-02-20 17:53:44 +1100

    reviewlesson

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ function subtract(a, b) {
 }
 function multiply(a, b) {
     return a * b;
-
 }
-
 function divide(a, b) {
     return a / b;
 }
-
 function increment(n) {
-    return n += 1
-}
-function decrement(n) {
-    return n -= 1
-}
-function makeInt(n) {
-    return parseInt(n, 10);
+    return n += 1;
+
 }
 
-function preserveDecimal(n) {
-    return parseFloat(n);
+
+function decrement(n) {
+    return n -= 1;
+
+
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string, 10);
 }
